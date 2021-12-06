534d48d4140c4d8e07cc3c4e35dc2d00cd69c1a6
冷罗KS <68413810+KSroido@users.noreply.github.com>
2021-12-06 21:02:32 +0800

    1

--- allocator.h
+++ allocator.h
@@ -1,10 +1,40 @@
+#ifndef _INC_STDDEF  //in stddef.h line 9
+#include<stddef.h>
+#endif
+
+#ifndef _MY_TINY_STL_ALLOCATOR //in case of Duplication
+#define _MY_TINY_STL_ALLOCATOR
 namespace TinyStl {
 
+template <typename T>
+class allocator
+{
+public:
+	typedef T				theType;
+	typedef T*				thePointer;
+	typedef const T*		theConstPointer;
+	typedef T&				theReference;
+	typedef const T			theConstReference;
+	typedef size_t			theSizeCounter;
+	typedef ptrdiff_t		theIndexDiffer;
 
-	template
+	static thePointer		allocate();
+	static thePointer		allocate(theSizeCounter n);
+	static void				deallocate(thePointer ptr);
+	static void				construct(thePointer ptr);
+
+
+
+
+
+
+
+};
+
 
 
 
 
 
 };//end of namespace TinyStl
+#endif //end of _MY_TINY_STL_ALLOCATOR
